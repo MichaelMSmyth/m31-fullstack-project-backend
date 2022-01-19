fdf4a2a501c066a5aca1a0f3b8a7f6a8d5586d87
Michael Smyth <michael.m.smyth@gmail.com>
2022-01-19 11:03:12 +0000

    copied template code from previous project and installed dependencies

---
+++ src/server.js
@@ -0,0 +1,16 @@
+require("./db/connection");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const movieRouter = require("./movie/movie.routes");
+const userRouter = require("./user/user.routes");
+
+app.use(express.json());
+app.use(cors());
+
+app.use(userRouter);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
