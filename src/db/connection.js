5c32635a3441b8ac9f908b16f3bb37a6e7cdd0e1
DanielRyanDev <daniel.jgryan2016@gmail.com>
2022-01-20 15:13:53 +0000

    resolved merges; added card to User.Model

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,17 +2,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connection = async () => {
-<<<<<<< HEAD
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Success");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-connection();
-=======
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Success")
@@ -22,4 +11,3 @@ connection();
 };
 
 connection();
->>>>>>> 51887941de9588481141d41287d8cd46a4102124
--- src/user/user.model.js
+++ src/user/user.model.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  card: {
-    type: String,
-  }
-});
-
-userSchema.methods.generateAuthToken = function () {
-  return jwt.sign({ _id: this._id }, process.env.SECRET, {
-    expiresIn: "365 Days",
-  });
-};
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
-=======
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const { string } = require("yargs");
@@ -49,9 +16,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    card: {
+    card: [{
         type: String,
-    }
+    }]
 })
 
 userSchema.methods.generateAuthToken = function () {
@@ -63,4 +30,3 @@ userSchema.methods.generateAuthToken = function () {
 const User = mongoose.model("User", userSchema)
 
 module.exports = User;
->>>>>>> 51887941de9588481141d41287d8cd46a4102124
